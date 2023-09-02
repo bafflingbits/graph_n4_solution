e049e96be073e176b78989d2a91adf7f1e2834a9
bafflingbits <143870286+bafflingbits@users.noreply.github.com>
2023-09-02 15:01:36 +0000

    Add patches, and adjust build script to use some by default

--- build_singular.sh
+++ build_singular.sh
@@ -1,5 +1,14 @@
-#!/bin/sh
-# tested this on an aws x86-64 machine running Ubuntu 20.04
+#!/bin/bash
+# confirmed to work on x86-64 machine running Ubuntu 22.04
+# have used similar script for arm64 machine running Ubuntu 22.04 on AWS,
+# and under x86-64 WSL (Windows Subsystem for Linux) Ubuntu 20.04
+set -e -u
+
+VERSION_TAG=Release-4-3-2p7
+
+NPROC=$(nproc)
+# don't hog the system
+NPROC=$(( NPROC >= 4 ? NPROC - 2 : 1 ))
 
 set -x
 
@@ -11,7 +20,11 @@ sudo apt-get install -y libgmp-dev libmpfr-dev libcdd-dev libntl-dev
 
 git clone https://github.com/Singular/Singular.git
 cd Singular
+git checkout $VERSION_TAG
 ./autogen.sh
 ./configure
-make -j 4
+git apply ../patches/debug_print_remaining.patch
+git apply ../patches/lift_early_detection.patch
+git apply ../patches/larger_bundles.patch
+make -j$NPROC
 
