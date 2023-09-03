3a89e77d9a6fb9ebfc244593a2b54a07b898208b
bafflingbits <143870286+bafflingbits@users.noreply.github.com>
2023-09-03 03:02:18 +0000

    run-Singular-script.sh : better directory handling, and use --no-rc to stop line with full path from being written to output

--- run-Singular-script.sh
+++ run-Singular-script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#SINGULAR=Singular  # use installed version
-#SINGULAR=./latest/bin/Singular  # use latest release tgz
 SINGULAR=./Singular/Singular/Singular  # use built from github
 TIME=/usr/bin/time # do not use shell builtin
 
@@ -11,10 +9,18 @@ if [[ "$#" != "1" ]]; then
   exit 1
 fi
 fname="$1"
-outname="out_${fname%.*}.txt"
 
+dirpart=$(dirname "$fname")
+filepart=$(basename "$fname")
+outname="$dirpart/out_${filepart%.*}.txt"
 echo "Saving results in: $outname"
-echo "+ $SINGULAR -q -e 2>&1 < \"$fname\"" | tee "$outname"
+
+# use full path to Singular, in case script is invoked from another directory
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+SINGULAR_FULLPATH=$(readlink -f "$SCRIPT_DIR/$SINGULAR")
+
+SINGULAR_OPTIONS="-q --no-rc -e"
+echo "+ $SINGULAR $SINGULAR_OPTIONS 2>&1 < \"$fname\"" | tee "$outname"
 $TIME -f "max memory(KB):%M  time(sec):%e = %E" \
-    $SINGULAR -q -e 2>&1 < "$fname" | tee -a "$outname"
+    $SINGULAR_FULLPATH $SINGULAR_OPTIONS 2>&1 < "$fname" | tee -a "$outname"
 
